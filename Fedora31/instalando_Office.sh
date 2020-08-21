0d19a19f59913dc8f5f484da8938d084ba61481a
Edivan Carvalho <edivancarvalho2008@hotmail.com>
2020-08-20 21:21:15 -0300

    Create instalando_Office.sh

---
+++ Fedora31/instalando_Office.sh
@@ -0,0 +1,7 @@
+#-- Usando o fedora 31 --#
+
+sudo wget http://rpm.playonlinux.com/playonlinux.repo -O /etc/yum.repos.d/playonlinux.repo
+#depois.
+dnf update
+dnf install jq
+dnf install playonlinux
