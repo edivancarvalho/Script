04b8c0c29922060da68d40b992226581fb3aa647
Edivan Carvalho <edivancarvalho2008@hotmail.com>
2020-09-06 21:11:06 -0300

    Update VirtualBox.sh

--- Fedora31/VirtualBox/VirtualBox.sh
+++ Fedora31/VirtualBox/VirtualBox.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+#
+# Instatação do virtualBox no Fedora 31;
+
+# Efetuar o donwload do virtual box no site oficial;
+
+# Dependecias;
+dnf install SDL
+dnf install kernel-devel kernel-devel-5.7.15-100.fc31.x86_64
+
+rpm -ivh VirtualBox-6.1-6.1.14_140239_fedora31-1.x86_64.rpm 
