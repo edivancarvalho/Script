a3c917ea1e368755014c9af5552b302b35bf09db
Edivan Carvalho <edivancarvalho2008@hotmail.com>
2020-10-28 12:46:53 -0300

    Create teste.sh

---
+++ teste.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+# teste de caracter 
+ú, á, à, Á, À, ã, Ã, í, pão, "Goiânia", "Goiás"
+
+
+Version: 1.49.2
+Commit: e5e9e69aed6e1984f7499b7af85b3d05f9a6883a
+Date: 2020-09-24T16:26:09.944Z
+Electron: 9.2.1
+Chrome: 83.0.4103.122
+Node.js: 12.14.1
+V8: 8.3.110.13-electron.0
+OS: Linux x64 5.8.11-100.fc31.x86_64 Cinnamon
+
